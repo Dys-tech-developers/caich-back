de617a2ba0b55cbe844b973f1dc4cb9af6cde3bd
joacodimaro97 <joacodimaro97@gmail.com>
2024-07-09 12:12:55 -0300

    add login-logout

--- routes/users/users.js
+++ routes/users/users.js
@@ -3,6 +3,8 @@ import createUser from '../../controllers/users/createUser.js';
 import { getUsers, getEntrenadores } from '../../controllers/users/getUser.js';
 import updateUserCategory from '../../controllers/users/updateCategoriaEntrenador.js';
 import createCategoria from '../../controllers/categorias/crearCategoria.js';
+import login from "../../controllers/users/login.js"
+import logout from "../../controllers/users/logout.js"
 const router = express.Router();
 
 
@@ -15,7 +17,8 @@ router.post("/create-user",createUser);
 router.get("/get-users", getUsers);
 router.get("/get-entrenadores", getEntrenadores)
 router.put("/update-categoria", updateUserCategory)
-
+router.post("/auth/login", login)
+router.post("/auth/logout", logout)
 //categoriaRoutes
 router.post("/crear-categoria", createCategoria)
 
