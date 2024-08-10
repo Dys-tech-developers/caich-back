37a94bf3bd809e2dda1700b02c914731b7ec791f
joacodimaro97 <joacodimaro97@gmail.com>
2024-08-09 21:58:15 -0300

    add filtros

--- controllers/jugadores/getJugadores.js
+++ controllers/jugadores/getJugadores.js
@@ -2,16 +2,12 @@ import Jugador from "../../models/Jugador.js";
 import Categoria from "../../models/Categoria.js";
 import { Op } from "sequelize";
 
-const getJugadores = async (req,res) => {
+const getJugadores = async (req, res) => {
     try {
-        const { nombre, categoria, dni } = req.query; 
-       //joacodimaro97@gmail.com agrega mostrar activos / inactivos
-        let where = {
-            [Op.or]: [
-              { estado: 'activo' },
-              { estado: 'inactivo' }
-            ]
-          };
+        const { nombre, categoria, dni, estado } = req.query;
+
+        // Establece el filtro por defecto, sin restricciones
+        let where = {};
 
         // Agregar filtro de nombre si se proporciona
         if (nombre) {
@@ -30,26 +26,28 @@ const getJugadores = async (req,res) => {
             where.categoria_id = categoria;
         }
 
-        // filtrar por estado
-        if (estado) {
-            where.estado = estado
+        // Agregar filtro de estado si se proporciona y no es una cadena vacía
+        if (estado !== undefined) {
+            if (estado !== "") {
+                where.estado = estado;
+            }
+            // Si `estado` es una cadena vacía, no aplicar filtro de estado
         }
-        
-        //joacodimaro97@gmail.com agrega nombre de categoria de c/jugador
+
+        // Agrega la inclusión de la categoría para obtener el nombre
         const jugadores = await Jugador.findAll({
             where,
             include: [{
-              model: Categoria,
-              attributes: ['nombre']
+                model: Categoria,
+                attributes: ['nombre']
             }]
-          });
+        });
 
-        res.status(200).json({jugadores})
+        res.status(200).json({ jugadores });
     } catch (error) {
         console.error('Error al obtener jugadores:', error);
         res.status(500).json({ error: 'Error interno en el servidor' });
     }
-
 }
 
 export default getJugadores;
--- controllers/jugadores/updateJugador.js
+++ controllers/jugadores/updateJugador.js
@@ -2,8 +2,8 @@ import Jugador from "../../models/Jugador.js";
 
 const updateJugador = async (req, res) => {
     const { id } = req.params;
-    const { telefono, tutor_telefono, categoria_id, numero_socio, qr, imagen, estado } = req.body;
-    
+    const { telefono, tutor_telefono, categoria_id, numero_socio, qr, imagen, estado, nombre, fecha_nacimiento } = req.body;
+    console.log("body", req.body)
     try {
         // Buscar el jugador por su ID
         const jugador = await Jugador.findByPk(id);
@@ -13,6 +13,8 @@ const updateJugador = async (req, res) => {
         }
 
         // Actualizar los campos permitidos
+        
+        jugador.nombre = nombre || jugador.nombre;
         jugador.telefono = telefono || jugador.telefono;
         jugador.tutor_telefono = tutor_telefono || jugador.tutor_telefono;
         jugador.categoria_id = categoria_id || jugador.categoria_id;
@@ -20,7 +22,7 @@ const updateJugador = async (req, res) => {
         jugador.qr = qr || jugador.qr;
         jugador.imagen = imagen || jugador.imagen;
         jugador.estado = estado || jugador.estado;
-
+        jugador.fecha_nacimiento = fecha_nacimiento || jugador.fecha_nacimiento
         // Guardar los cambios
         await jugador.save();
 
